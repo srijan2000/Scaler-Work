5eb0cadb88096b128cccc021b3499ec36c04da3b
srijanv <srijan.veerareddy@gmail.com>
2023-12-19 13:09:14 -0500

    Scaler Work Binary Tree

---
+++ src/main/java/com/srijan/BinaryTree.java
@@ -0,0 +1,4 @@
+package com.srijan;
+
+public class BinaryTree {
+}
---
+++ src/main/java/com/srijan/Main.java
@@ -0,0 +1,7 @@
+package com.srijan;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
