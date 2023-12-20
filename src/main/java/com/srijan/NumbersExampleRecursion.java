2ea5ab25dca1f31051bbb1c293f517dad2c5ee04
srijanv <srijan.veerareddy@gmail.com>
2023-12-19 20:49:52 -0500

    Recurrison

--- src/main/java/com/srijan/NumbersExampleRecursion.java
+++ src/main/java/com/srijan/NumbersExampleRecursion.java
@@ -1,32 +1,18 @@
 package com.srijan;
 
 public class NumbersExampleRecursion {
-    // when a function finishes executing, it is removed from stack and the flow of program is restored to where that function was called
-
     public static void main(String[] args) {
         //write a function that takes in a number and prints it
         //print first 5 number : 1 2 3 4 5
-        print1(1);
+        print(1);
 
     }
-    static void print1(int n){
+    static void print(int n){
+        if(n == 6){
+            return;
+        }
         System.out.println(n);
-        print2(2);
+        print(n+1);
     }
-    static void print2(int n){
-        System.out.println(n);
-        print3(3);
-    }
-    static void print3(int n){
-        System.out.println(n);
-        print4(4);
 
-    }
-    static void print4(int n){
-        System.out.println(n);
-        print5(5);
-    }
-    static void print5(int n){
-        System.out.println(n);
-    }
 }
