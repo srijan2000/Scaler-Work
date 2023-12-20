09fa10662e555589d4c24226bb375cfd81d32c80
srijanv <srijan.veerareddy@gmail.com>
2023-12-20 11:48:46 -0500

    Recurrison

--- src/main/java/com/srijan/NumbersExampleRecursion.java
+++ src/main/java/com/srijan/NumbersExampleRecursion.java
@@ -4,10 +4,14 @@ public class NumbersExampleRecursion {
     public static void main(String[] args) {
         //write a function that takes in a number and prints it
         //print first 5 number : 1 2 3 4 5
+
         print(1);
 
-    }
+    }//you can convert recursion solution into iteration and vice versa
+    //space complexity is not constant because of recursive calls
+
     static void print(int n){
+        //Base condition in Recursion : Condition where our recursion will stop making new calls
         if(n == 6){
             return;
         }
