cbb8d7b4a3e591c887ca2d864437c5b92d92c7d3
srijanv <srijan.veerareddy@gmail.com>
2023-12-19 20:05:05 -0500

    Recurrison

--- src/main/java/com/srijan/Main.java
+++ src/main/java/com/srijan/Main.java
@@ -2,6 +2,10 @@ package com.srijan;
 
 public class Main {
     public static void main(String[] args) {
+
+        message();
+    }
+    static void message(){
         System.out.println("Hello world!");
     }
 }
