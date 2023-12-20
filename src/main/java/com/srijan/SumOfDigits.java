878ef8d8e2924e273e69cdeb4d8ab124ecab039c
srijanv <srijan.veerareddy@gmail.com>
2023-12-20 16:45:28 -0500

    Recurrison

---
+++ src/main/java/com/srijan/SumOfDigits.java
@@ -0,0 +1,20 @@
+package com.srijan;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println(print(98613 , 0));
+
+    }
+    static int print(int n, int sum ){
+        if(n<=0){
+            return sum;
+        }
+
+        int reminder = n %10;
+        sum = sum + reminder;
+        n = n/10;
+
+
+        return print (n, sum);
+    }
+}
