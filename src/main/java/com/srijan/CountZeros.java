802baa60a04f4f43c8a48f6add8db58ef5c7d7fb
srijanv <srijan.veerareddy@gmail.com>
2023-12-21 11:12:57 -0500

    Recurrison

---
+++ src/main/java/com/srijan/CountZeros.java
@@ -0,0 +1,19 @@
+package com.srijan;
+
+public class CountZeros {
+    static int count = 0;
+    static int count(int n){
+        if(n%10 == n) {
+            return count;
+        }
+        if(n% 10 == 0){
+            count++;
+        }
+       return count(n/10);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count(8000900));
+    }
+}
