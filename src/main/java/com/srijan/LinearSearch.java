4eb52b95abb2f7eab1c28c89b79a500f0f94a532
srijanv <srijan.veerareddy@gmail.com>
2023-12-22 13:09:14 -0500

    Recurrison

--- src/main/java/com/srijan/LinearSearch.java
+++ src/main/java/com/srijan/LinearSearch.java
@@ -2,15 +2,15 @@ package com.srijan;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int target = 9;
+        int arr[] = {3, 2, 1, 18, 9};
+        int target = 54;
         System.out.println( search(arr, target, 0));
 
 
     }
     static int search (int arr[], int target, int i){
         if(i == arr.length-1 && target!= arr[i]){
-            return 0;
+            return -1;
         }
         if(target != arr[i]){
             return search(arr, target, i+1);
