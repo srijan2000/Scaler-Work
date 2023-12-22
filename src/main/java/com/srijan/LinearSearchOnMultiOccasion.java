3cc3378cd114fab5bf11fbe44c5372ea194811ff
srijanv <srijan.veerareddy@gmail.com>
2023-12-22 14:54:03 -0500

    Recurrison

--- src/main/java/com/srijan/LinearSearchOnMultiOccasion.java
+++ src/main/java/com/srijan/LinearSearchOnMultiOccasion.java
@@ -21,7 +21,7 @@ public class LinearSearchOnMultiOccasion {
        findAllIndex(arr, target, i + 1);
 
    }*/
-    public static void main(String[] args) {
+  /*  public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5, 6, 6, 7, 6, 9};
         int target = 6;
 
@@ -38,6 +38,20 @@ public class LinearSearchOnMultiOccasion {
             list.add(i);
         }
         searAllIndex(arr, target, i+1);
+    }*/
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list){
+        if(index == arr.length){
+            return list;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index+1, list);
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {2, 3, 1, 4, 4, 5};
+        System.out.println(findAllIndex(arr, 4, 0, new ArrayList<>()));
     }
 
 }
